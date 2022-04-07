249fa42a19b835f3d52a67112bcade6994bfd6b2
Rhodes-F <102707768+Rhodes-F@users.noreply.github.com>
2022-04-07 16:40:44 -0500

    changed names

--- HW2.X/main.c
+++ HW2.X/main.c
@@ -1,9 +1,5 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
-#include<stdio.h>
-
-#define NU32_DESIRED_BAUD 230400    // Baudrate for RS232
-#define NU32_SYS_FREQ 48000000ul    // 48 million Hz
 
 // DEVCFG0
 #pragma config DEBUG = OFF // disable debugging
@@ -36,62 +32,6 @@
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 
-char msg[500];
-
-void ReadUART1(char * message, int maxLength) {
-  char data = 0;
-  int complete = 0, num_bytes = 0;
-  // loop until you get a '\r' or '\n'
-  while (!complete) {
-    if (U1STAbits.URXDA) { // if data is available
-      data = U1RXREG;      // read the data
-      if ((data == '\n') || (data == '\r')) {
-        complete = 1;
-      } else {
-        message[num_bytes] = data;
-        ++num_bytes;
-        // roll over if the array is too small
-        if (num_bytes >= maxLength) {
-          num_bytes = 0;
-        }
-      }
-    }
-  }
-  // end the string
-  message[num_bytes] = '\0';
-}
-
-// Write a character array using UART3
-void WriteUART1(const char * string) {
-  while (*string != '\0') {
-    while (U1STAbits.UTXBF) {
-      ; // wait until tx buffer isn't full
-    }
-    U1TXREG = *string;
-    ++string;
-  }
-}
-
-void StartUART1(){
-    __builtin_disable_interrupts(); // disable interrupts while initializing things
-    // UART ?
-    U1MODEbits.BRGH = 0; // set baud to NU32_DESIRED_BAUD
-    U1BRG = ((NU32_SYS_FREQ / NU32_DESIRED_BAUD) / 16) - 1;
-
-    // 8 bit, no parity bit, and 1 stop bit (8N1 setup)
-    U1MODEbits.PDSEL = 0;
-    U1MODEbits.STSEL = 0;
-
-    // configure TX & RX pins as output & input pins
-    U1STAbits.UTXEN = 1;
-    U1STAbits.URXEN = 1;
-    // configure hardware flow control using RTS and CTS
-    U1MODEbits.UEN = 2;
-
-    // enable the uart
-    U1MODEbits.ON = 1;
-}
-
 int main() {
 
     __builtin_disable_interrupts(); // disable interrupts while initializing things
@@ -110,14 +50,9 @@ int main() {
 
     // do your TRIS and LAT commands here
     TRISAbits.TRISA4 = 0 ;// output
-    LATAbits.LATA4 = 0; //high
+    LATAbits.LATA4 = 1; //high
     TRISBbits.TRISB4 = 1; // input
-    
-    // uart stuff
-    StartUART1();
-    U1RXRbits.U1RXR = 0b0000; // Set A2 to U1RX
-    RPB3Rbits.RPB3R = 0b0001; // Set B3 to U1TX
-    int i = 0;
+      
 
     __builtin_enable_interrupts();
 
@@ -125,9 +60,6 @@ int main() {
         // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
         // remember the core timer runs at half the sysclk
         if (PORTBbits.RB4 == 0){
-            i+=1;
-            sprintf(msg, "Button!!! %d \r\n", i);
-			WriteUART1(msg);
             _CP0_SET_COUNT(0);                      
             LATAbits.LATA4 = 1;                     
             while (_CP0_GET_COUNT() <= 12000000) {     
